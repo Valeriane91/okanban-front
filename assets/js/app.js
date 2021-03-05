3c873c79e7b06e2e0df56879b9506633100630c2
olivier <olivier@oclock.io>
2021-03-05 09:36:54 +0100

    etape 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,30 @@ var app = {
   // fonction d'initialisation, lancée au chargement de la page
   init: function () {
     console.log('app.init !');
+    app.addListenerToActions();
+  },
+
+  /**
+   * Ajoute des écouteurs d'évènements sur les éléments de la pages.
+   */
+  addListenerToActions: () => {
+    // On récupère notre bouton pour lui attacher un écouteur d'évenements
+    const buttonElement = document.getElementById('addListButton');
+    // console.log(buttonElement);
+    // attacher l'écouteur d'évenement à mon bouton
+    buttonElement.addEventListener('click', app.showAddListModal);
+  },
+
+  /**
+   * Affiche la modale en réaction au click sur le bouton "Ajouter une liste".
+   * 
+   * @param { Event } event - Les infos sur l'évenement en cours.
+   */
+  showAddListModal: (event) => {
+    // console.log("Log de l'event :  ", event);
+    const modale = document.getElementById('addListModal');
+    // console.log(modale);
+    modale.classList.add('is-active');
   }
 
 };
