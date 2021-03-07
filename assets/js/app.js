ec6b3e4945d5d1942a3eb0d8efe60a4e82f9393d
valeriane91 <jean.valerieanne@gmail.com>
2021-03-07 01:59:30 +0100

    merge from correction

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,24 +5,40 @@ var app = {
   // fonction d'initialisation, lancée au chargement de la page
   init: function (addListenerToActions) {
     console.log('app.init !', addListenerToActions);
+    //une fois qu'on a fait une fonction il faut l'appeler
+    app.addListenerToActions();
   }
   // Ajout d'une méthode addListenerToActions et récupérer le bouton "ajouter une liste"
-  addListenerToActions: function () {
-    var button = document.getElementById("addListButton");
-    button.addEventListener("click", app.showAddListModal);
+  addListenerToActions: () => {
+    const buttonElement = document.getElementById('addListButton');
+    //console.log(buttonElement);
+    buttonElement.addEventListener("click", app.showAddListModal);
 
-    var bouton = document.querySelectorAll("#close");
-    bouton.addEventListener("click", app.hideModals);
+    // gestion des bouton de fermeture de la modale
+
+    const bouton = document.querySelectorAll('.modal .close'); // les elements qui ont la classe modal et la classe close avec queryselector
+    //console.log(bouton);
+    bouton.forEach(buts) => { 
+    buts.addEventListener("click", app.hideModals);};
+    // bouton ce n'est pas un élément html mais une liste
+    //donc il faut faire un for each
+
+
     
     //récupérer le bon formulaire grâce à document.querySelector
-    var formulaire = document.querySelector("#input");
+    const formulaire = document.querySelector("#input");
     formulaire.addEventListener("submit", app.handleAddListForm);
   
-  }
 
-  handleAddListForm: function(event) {
-    document.getElementById("").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+
+  handleAddListForm: (event) => {
+
+   // document.getElementById("").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
     event.preventDefault();
+    console.log(event.target);// target c'est qui a déclencher l'élément
+    const formData = new FormData(evant.target);//va consulter le contenu de chaque formulaire et le mettre a disposition
+
+
 
   }
   // coder hideModals qui enlève la classe "is-active" à toutes les modales
@@ -34,9 +50,9 @@ var app = {
   }
 
   //Ajout de la méthode showAddListModal
-  showAddListModal: function (){
+  showAddListModal: (event) => {
     var addModal = document.getElementById("addListModal");
-    addModal;
+    addModal.classList.add('is-active');
   
   }
 
@@ -48,7 +64,7 @@ var app = {
 
     var template = document.getElementById('my-paragraph');
     var templateContent = template.content;
-    document.body.appendChild(templateContent);
+    //document.body.appendChild(templateContent);
   
   }
 
