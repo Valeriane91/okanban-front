07ae1357ff3037b3413f18c956e3c4fc6b13fe63
olivier <olivier@oclock.io>
2021-03-08 12:00:26 +0100

    gestion des POST card + list

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,7 +38,7 @@ var app = {
       // pour chaque carte reçue via l'API
       for (const card of cards) {
           //  on veut créer un bloc de HTML et l'insérer dans la bonne liste
-          app.makeCardInDOM(card.title, card.list_id, card.id);
+          app.makeCardInDOM(card.title, card.list_id, card.id, card.color);
       }
 
     }
@@ -154,7 +154,7 @@ var app = {
    * 
    * @param { Event } event - Les infos sur l'évenement qui a déclanché l'appel.
    */
-  handleAddListForm: (event) => {
+  handleAddListForm: async (event) => {
     // On empeche le formulaire de faire une requete POST par lui même.
     // Et donc en empeche la page de se recharger.
     event.preventDefault();
@@ -163,17 +163,78 @@ var app = {
     const formData = new FormData(event.target);
     // console.log(formData.get('name'));
 
-    app.makeListInDOM(formData.get('name'));
+    // Envoie d'une requete POST vers l'API pour enregitrer la liste en BDD
+    // On peut directement transmettre l'objet formData à fetch
+    // pour qu'il envoie les données à l'API avec la requete POST.
+    const paramRequest = {
+      method: 'POST',
+      body: formData
+    };
+
+    try {
+
+      const response = await fetch(`${app.base_url}/lists`, paramRequest);
+      console.log(response);
+
+      // On teste si le code de retour de l'API est "positif"
+      // si la requete à pu être traitée par le serveur
+      // response.ok vaut true
+      if (response.ok) {
+        
+        const list = await response.json();
+      
+        // On créé la liste avec le retour de l'API
+        app.makeListInDOM(list.name, list.id);
+      }
+      else {
+        const error = await response.json();
+        throw error;
+      }
+      
+    } catch (error) {
+      // l'abscence de réponse de l'API sera par exemple capturée par le catch
+      console.log("Error capturée par catch : ", error);
+    }
 
   },
 
-  handleAddCardForm: (event) => {
+  /**
+   * Gère la soumission du formulaire de création. d'une carte
+   * @param { Event } event 
+   */
+  handleAddCardForm: async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
+    const listId = formData.get('list_id');
+    // solution temporaire pour gérer la position
+    // TODO : gérer les positions de façon plus propre
+    const nbOfCards = document.querySelectorAll(`[data-list-id='${listId}'] .box`).length;
+    formData.append('position', nbOfCards + 1);
 
-    // console.log(formData.get('title'));
-    app.makeCardInDOM(formData.get('title'), formData.get('list_id'));
+    try {
+
+      // On envoie les donnéees de la carte vers l'API
+      const paramRequest = {
+        method: 'POST',
+        body: formData
+      };
+      const response = await fetch(`${app.base_url}/cards`, paramRequest);
+
+      if (response.ok) {
+        const card = await response.json();
+        // On créé la liste avec le retour de l'API
+        app.makeCardInDOM(card.title, card.list_id, card.id, card.color);
+      }
+      else {
+        const error = await response.json();
+        throw error;
+      }
+      
+    } catch (error) {
+      // l'abscence de réponse de l'API sera par exemple capturée par le catch
+      console.log(error);
+    }
     
   },
 
@@ -216,8 +277,10 @@ var app = {
    * @param { String } title - titre de la carte 
    * @param { Number } listId - ID de la liste dans laquelle on va insérer la carte 
    * @param { Number } cardId - ID de la carte 
+   * @param { String } cardColor - couleur de la carte
+
    */
-  makeCardInDOM: (title, listId, cardId) => {
+  makeCardInDOM: (title, listId, cardId, cardColor) => {
 
     // Récupérer le template, 
     const template = document.querySelector('#cardTemplate');
@@ -227,12 +290,18 @@ var app = {
     // on insére le titre de la nouvelle carte dans le HTML de la nouvelle carte 
     newCard.querySelector('.card__title').textContent = title;
     // console.log(newCard);
+    // console.log(cardColor);
     newCard.querySelector('.box').setAttribute('data-card-id', cardId);
+    // newCard.querySelector('.box').style.backgroundColor = cardColor;
+    // alternative
+    // newCard.querySelector('.box').setAttribute("style",`background-color: ${cardColor}`);
+
+    newCard.querySelector('.box').style.border = `2px solid ${cardColor}`;
 
     // // Insérer la nouvelle carte dans le DOM au bon endroit ! 
     // Le bon endroit est la liste qui a pour attribut data-list-id la valeur
     // passé en parametre dans la variable listId
-    console.log(listId);
+    // console.log(listId);
     document.querySelector(`[data-list-id="${listId}"] .panel-block`).appendChild(newCard);
 
     // // Je referme la modale
